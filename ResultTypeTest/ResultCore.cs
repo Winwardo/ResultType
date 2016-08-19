60315335e035c9cbb807e3ff11ae93db5e9c5526
Topher Winward <hi@topher.io>
2016-08-20 00:23:50 +0100

    Test more isOk and isError

--- ResultTypeTest/ResultCore.cs
+++ ResultTypeTest/ResultCore.cs
@@ -15,5 +15,26 @@ namespace ResultTypeTest
             var result = SimpleResult.Ok(5);
             Assert.IsTrue(result.isOk());
         }
+
+        [TestMethod]
+        public void AnError_IsOk_ReturnsFalse()
+        {
+            var result = SimpleResult.Error("error");
+            Assert.IsFalse(result.isOk());
+        }
+
+        [TestMethod]
+        public void AnOk_IsError_ReturnsFalse()
+        {
+            var result = SimpleResult.Ok(5);
+            Assert.IsFalse(result.isError());
+        }
+
+        [TestMethod]
+        public void AnError_IsError_ReturnsTrue()
+        {
+            var result = SimpleResult.Error("error");
+            Assert.IsTrue(result.isOk());
+        }
     }
 }
